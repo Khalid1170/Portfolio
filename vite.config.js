ea54001907ac8f448cf9d21220b52d5933ad2ce5
Khalid Abdi <khalidabdi321@gmail.com>
2026-06-08 23:38:36 +0100

    webpp

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
 import path from 'path';
-import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), imagetools()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
