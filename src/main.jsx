0d17464f1c15da02c68a96c0ae15a8d10fd1e41c
Khalid Abdi <khalidabdi321@gmail.com>
2026-06-08 23:53:59 +0100

    loading

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const loader = document.getElementById('initial-loader')
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+requestAnimationFrame(() => {
+  requestAnimationFrame(() => {
+    if (loader) {
+      loader.classList.add('hide')
+      setTimeout(() => loader.remove(), 400)
+    }
+  })
+})
